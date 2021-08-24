5b045d42c7927136bebdcfdf0fc970b0d54e1ea8
Lucas C. Noman <lucas.noman7@gmail.com>
2021-08-24 12:17:19 -0300

    chore: inital commit with section 2 exercise done and migrations created

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+  public class CustomersController : Controller
+  {
+    // GET: Customers
+    public ActionResult Index()
+    {
+      return View(putCustomersOnViewList());
+    }
+
+    public ActionResult Details(int id)
+    {
+      var customer = setCustomers().SingleOrDefault(c => c.Id == id);
+
+      if (customer == null)
+      {
+        return HttpNotFound();
+      }
+
+      return View();
+    }
+
+    private List<Customer> setCustomers()
+    {
+      return new List<Customer>()
+      {
+        new Customer {Id = 1, Name = "Joaozinho"},
+        new Customer {Id = 2, Name = "Mariazinha"}
+      };
+    }
+
+    private RandomMovieViewModel putCustomersOnViewList()
+    {
+      var customerList = new RandomMovieViewModel()
+      {
+        Customers = setCustomers()
+      };
+
+      return customerList;
+    }
+  }
+}
---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+  public class MoviesController : Controller
+  {
+    // GET: Movies/Random
+    public ActionResult Random()
+    {
+      return View(putMoviesOnViewList());
+    }
+
+    private List<Movie> setMovies()
+    {
+      return new List<Movie>()
+      {
+        new Movie {Name = "Shrek"},
+        new Movie {Name = "Planeta dos Macacos"}
+      };
+    }
+
+    private RandomMovieViewModel putMoviesOnViewList()
+    {
+      return new RandomMovieViewModel()
+      {
+        Movie = setMovies()
+      };
+    }
+  }
+}
